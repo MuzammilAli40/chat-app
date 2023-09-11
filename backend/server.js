c15fcc7e671bfd32d8edd544f3b6f8b53b1ebedb
MuzammilAli40 <raomuzamil40@gmail.com>
2023-09-11 19:48:03 +0500

    Deploying to heroku

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,9 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+app.get('/', (req, res) => {
+   res.send('Server connected.')
+})
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server started in PORT ${PORT}`.yellow.bold));
